9309c2c17c20a7cc1f846fd04e791910522a1f4f
Trowbridge <ptrowbridge@hc-comapnies.com>
2017-02-08 17:35:21 -0500

    add outstanding payroll checks and parsing of bank data

---
+++ SQL/PG/Payroll Checks Outstanding.sql
@@ -0,0 +1,82 @@
+﻿
+WITH 
+	ADPC (
+		adp_comp, 
+		employee, 
+		pay_date, 
+		perdion_end, 
+		checkn, 
+		cms_acct, 
+		amount
+	) AS
+	(
+		SELECT
+			adp_comp,
+			employee,
+			to_date(pay_date,'YYMMDD') pay_date, 
+			period_end,
+			checkn::numeric,
+			cms_acct,
+			amount
+		FROM
+			payroll.adp_rp 
+		WHERE
+			gl_descr = 'NET PAY'
+	),
+	BANK (
+		aodate,
+		trans,
+		acctn,
+		refr,
+		checkn,
+		amount
+	) AS
+	(
+		SELECT
+			(rec->>'AsOfDate')::Date,
+			rec->>'Transaction',
+			rec->>'AccountName',
+			rec->>'Reference',
+			CHK[1]::numeric,
+			(rec->>'Amount')::numeric
+		FROM
+			tps.trans 
+			LEFT JOIN LATERAL regexp_matches(rec->>'Reference','([^''0].*)','g') CHK ON TRUE
+		WHERE
+			srce = 'PNCC' AND
+			rec @> $${"Transaction":"Checks Paid","AccountName":"The HC Operating Company PAYR"}$$
+		ORDER BY
+			CHK[1]::numeric asc
+	)
+
+SELECT
+	ADP_COMP,
+	PAY_DATE,
+	SUM(ADP+BANK) OVER (ORDER BY ADP_COMP, PAY_DATE),
+	SUM(BANK) OVER (ORDER BY ADP_COMP, PAY_DATE)
+FROM
+	(
+		SELECT
+			ADP_COMP, 
+			PAY_DATE, 
+			SUM(AMOUNT) ADP,
+			SUM(BAMOUNT) BANK
+		FROM
+			(
+				SELECT
+					ADPC.*,
+					BANK.AODATE,
+					BANK.AMOUNT BAMOUNT
+				FROM
+					ADPC
+					LEFT JOIN BANK ON
+							BANK.CHECKN = ADPC.CHECKN AND
+							BANK.AMOUNT + ADPC.AMOUNT = 0
+			) X
+		GROUP BY	
+			ADP_COMP,
+			PAY_DATE
+		ORDER BY
+			ADP_COMP,
+			PAY_DATE
+	) S
--- SQL/PG/TPS - Parse PNC Description.sql
+++ SQL/PG/TPS - Parse PNC Description.sql
@@ -1,7 +1,8 @@
 ﻿select 
 	unq,
 	REC->>'Description' descr,
-	regexp_matches(rec->>'AccountName','(\w{5})$') acct,
+	rec->>'AccountName' acct,
+	rec->>'Transaction' trans,
 	ini[1],
 	--bb.*,
 	rfb[1],
@@ -19,18 +20,18 @@ from
 	tps.trans 
 	--LEFT JOIN LATERAL regexp_matches(rec->>'Description','([\w/]+?):(.+?) [\w/]+?:','g') bb ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','RFB:(.+?)(?=$|[\w/]+?:)') rfb ON TRUE
-	LEFT JOIN LATERAL regexp_matches(rec->>'Description','OBI:(.+?)(?=$|[\w/]+?:)') obi ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','OBI:(.+?) [A-Z]+?:') obi ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','ORIGINATOR:(.+?) AC/') ori ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','DATE:(\d+)') dat ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','TIME:(\d+)') tim ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','BENEFICIARY:(.+?) AC/\d*(.*?)\s[\w/]+?:') bene ON TRUE
-	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Comp Name:(.+?) Comp') compn ON TRUE
-	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Cust Name:(.+?) Adden') custn ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Comp Name:(.+?)(?=$| Comp|\w+?:)') compn ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Cust Name:(.+?)(?=$|\w+?:)') custn ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','SEC:(.+?) Cust') sec ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Desc:(.+?) Comp') descr ON TRUE
-	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Discr:(.+?) SEC:') discr ON TRUE
-	LEFT JOIN LATERAL regexp_matches(rec->>'Description','([\w].*?)(?=$| - |\s[0-9].*?|[\w/]+?:)') ini ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Discr:(.+?)(?=$| SEC:|\w+?:)') discr ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','([\w].*?)(?=$| -|\s[0-9].*?|[\w/]+?:)') ini ON TRUE
 WHERE
 	srce = 'PNCC' AND
-	rec->>'AsOfDate' >= '2016-12-01'
+	rec->>'AsOfDate' >= '2016-10-01'
 order by ini[1] asc
