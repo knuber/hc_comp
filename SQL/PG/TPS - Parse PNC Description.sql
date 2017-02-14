529aa652b5e7b213d8b4f34fcf348a1e0c8f1cf4
Trowbridge <ptrowbridge@hc-comapnies.com>
2017-02-14 16:29:34 -0500

    add non-atomic map, trans parser updates

--- SQL/PG/TPS - Parse PNC Description.sql
+++ SQL/PG/TPS - Parse PNC Description.sql
@@ -11,11 +11,22 @@
 	bene[1],
 	compn[1],
 	custn[1],
+	custid[1],
+	adp_comp[1],
 	sec[1],
 	descr[1],
 	discr[1],
 	dat[1],
-	tim[1]
+	tim[1],
+	chk[1],
+	adde[1],
+	curr[1] cd1,
+	curr[3] cd2,
+	curr[2] cr1,
+	curr[4] cr2,
+	acct.rv[1] ac1,
+	acct.rv[2] ac2
+	
 from 
 	tps.trans 
 	--LEFT JOIN LATERAL regexp_matches(rec->>'Description','([\w/]+?):(.+?) [\w/]+?:','g') bb ON TRUE
@@ -27,11 +38,17 @@ from
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','BENEFICIARY:(.+?) AC/\d*(.*?)\s[\w/]+?:') bene ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Comp Name:(.+?)(?=$| Comp|\w+?:)') compn ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Cust Name:(.+?)(?=$|\w+?:)') custn ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Cust ID:(.+?)(?=$|\w+?:)') custid ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Cust ID:.*?(B3X|UDV|U7E|U7C|U7H|U7J).*?(?=$|\w+?:)') adp_comp ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','SEC:(.+?) Cust') sec ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Desc:(.+?) Comp') descr ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Discr:(.+?)(?=$| SEC:|\w+?:)') discr ON TRUE
 	LEFT JOIN LATERAL regexp_matches(rec->>'Description','([\w].*?)(?=$| -|\s[0-9].*?|\s[\w/]+?:)') ini ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Reference','([^''0].*)','g') CHK ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','Addenda:(.+?)(?=$|\w+?:)') adde ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','.*(DEBIT|CREDIT).*(USD|CAD).*(DEBIT|CREDIT).*(USD|CAD).*') curr ON TRUE
+	LEFT JOIN LATERAL regexp_matches(rec->>'Description','AC/(\w* ).*AC/(\w* )',) acct(rv) ON TRUE
 WHERE
-	srce = 'PNCC' AND
-	rec->>'AsOfDate' >= '2016-10-01'
+	srce = 'PNCC'
 order by ini[1] asc
+LIMIT 1000
---
+++ SQL/PG/TPS - map_all_grouped_notatom.sql
@@ -0,0 +1,85 @@
+﻿SELECT
+	d.srce,
+	d.target,
+	d.retval,
+	jsonb_agg(d.rec_key) rec_key,
+	tps.jsonb_obj_agg_ignore_null(d.value_map) value_map,
+	tps.jsonb_obj_agg_ignore_null(d.direct_map) direct_map,
+	tps.jsonb_obj_agg_ignore_null(d.value_map) || tps.jsonb_obj_agg_ignore_null(d.direct_map) final_value_map
+FROM
+	--select only distinct records so that we only see unique trans records in the rec_key column
+	(
+	SELECT 
+		u.target,
+		u.retval retval,
+		u.srce,
+		u.rkey rec_key,
+		v.map value_map,
+		m.map direct_map
+	FROM 	
+		--re-aggregate return values and explude any records where one or more regex failed with a null result
+		(
+		SELECT 
+			x.srce,
+			x.target,
+			x.unq, 
+			tps.jsonb_obj_agg_null_atomic(x.rkey) rkey,
+			tps.jsonb_obj_agg_null_atomic(x.retval) AS retval
+		FROM 
+			--unwrap json instruction and apply regex using a count per original line fro re-aggregation
+			( 
+			SELECT 
+				m.srce,
+				m.target,
+				t.unq,
+				jsonb_build_object(
+					e.v ->> 'key'::text,
+					(t.rec -> (e.v ->> 'key'::text))
+				) AS rkey,
+				--array_to_json(mt.mt)::jsonb AS retval,
+				jsonb_build_object(e.v->>'field',array_to_json(mt.mt)) retval
+			FROM 
+				tps.map_rm m
+				LEFT JOIN LATERAL jsonb_array_elements(m.regex->'where') w(v) ON TRUE
+				JOIN tps.trans t ON 
+					t.srce = m.srce AND
+					t.rec @> w.v
+				LEFT JOIN LATERAL jsonb_array_elements(m.regex->'defn') WITH ORDINALITY e(v, rn) ON true
+				LEFT JOIN LATERAL regexp_matches(t.rec ->> (e.v ->> 'key'::text), e.v ->> 'regex'::text) mt(mt) ON true
+			ORDER BY 
+				m.srce, 
+				m.target, 
+				t.unq, 
+				e.rn
+			) x
+		GROUP BY 
+			x.srce, 
+			x.target, 
+			x.unq
+		HAVING
+			tps.jsonb_obj_agg_null_atomic(x.retval) IS NOT NULL
+			
+		) u
+		LEFT OUTER JOIN tps.map_rv v ON
+			v.target = u.target AND
+			v.srce = u.srce AND
+			v.retval = u.retval
+		LEFT OUTER JOIN tps.map m ON
+			m.srce = u.srce AND
+			m.item = u.rkey
+	GROUP BY 
+		u.srce, 
+		u.target, 
+		u.retval,
+		u.rkey,
+		v.map,
+		m.map
+	ORDER BY 
+		u.srce, 
+		u.target, 
+		u.retval
+	) d
+GROUP BY
+	d.srce,
+	d.target, 
+	d.retval;
