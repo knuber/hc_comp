0fc1d6068c496869f4821b6c6abe3358bac8efd2
Trowbridge <ptrowbridge@hc-comapnies.com>
2017-04-04 14:02:05 -0400

    delete files moved to other repos

--- JavaScript/test_http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('node is on\n');
-}).listen(8082);
-
-console.log('Server running on port 8082.');
