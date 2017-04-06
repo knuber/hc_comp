f4353d45b4ca1a335bccf68e3e6267a8eb21dc4c
Trowbridge <ptrowbridge@hc-comapnies.com>
2017-04-06 12:52:25 -0400

    convert array to values table and reformat case statements to one line

--- SQL/PG/Payroll - Withholding Disbursement Report.sql
+++ SQL/PG/Payroll - Withholding Disbursement Report.sql
@@ -2,53 +2,19 @@ SELECT
 	PAY_DATE,
 	PMNT,
 	CASE X.JRNLT WHEN 'PRIMAR' THEN CODE ELSE 'TOTALS' END CODE,
-	SUBSTR(
-	    CASE x.jrnlt
-			WHEN 'PRIMAR' THEN r.cms_acct
-			ELSE COALESCE(r.cms_tb, '00') || '0000101002'
-	    END, 7, 6) acct,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'B3X') B3X,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'UDV') UDV,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'U7H') U7H,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'U7J') U7J,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'U7C') U7C,
-	SUM(r.amount *
-	    CASE x.jrnlt
-			WHEN 'PRIMAR'::text THEN '-1'::integer
-			ELSE 1
-	    END::numeric
-	) FILTER (WHERE ADP_COMP = 'U7E') U7E
+	SUBSTR(CASE x.jrnlt WHEN 'PRIMAR' THEN r.cms_acct ELSE COALESCE(r.cms_tb, '00') || '0000101002' END, 7, 6) acct,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'B3X') B3X,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'UDV') UDV,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'U7H') U7H,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'U7J') U7J,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'U7C') U7C,
+	SUM(r.amount * CASE x.jrnlt WHEN 'PRIMAR'::text THEN '-1'::integer ELSE 1 END::numeric) FILTER (WHERE ADP_COMP = 'U7E') U7E
 FROM 
 	payroll.adp_rp r
 	INNER JOIN payroll.adp_code c on
 		r.gl_descr::text = c.code::text AND 
 		r.prim_offset::text = c.po::text
-	CROSS JOIN unnest(ARRAY['PRIMAR'::text, 'OFFSET'::text]) x(jrnlt)
+	CROSS JOIN (VALUES('PRIMAR'::text, 'OFFSET'::text)) x(jrnlt)
 	
 WHERE 
 	r.gl_descr::text = c.code::text AND 
@@ -60,11 +26,7 @@ GROUP BY
 	PAY_DATE,
 	PMNT,
 	CASE X.JRNLT WHEN 'PRIMAR' THEN CODE ELSE 'TOTALS' END,
-	SUBSTR(
-	    CASE x.jrnlt
-		WHEN 'PRIMAR' THEN r.cms_acct
-		ELSE COALESCE(r.cms_tb, '00') || '0000101002'
-	    END, 7, 6)
+	SUBSTR(CASE x.jrnlt WHEN 'PRIMAR' THEN r.cms_acct ELSE COALESCE(r.cms_tb, '00') || '0000101002' END, 7, 6)
 ORDER BY
 	PAY_DATE ASC,
-	PMNT ASC
+	PMNT ASC;
